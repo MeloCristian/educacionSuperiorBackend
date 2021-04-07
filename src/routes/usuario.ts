f1f383d7dce93c7038cac9ad9b358e22df7762f6
ZorcrownUser <zorcrownuser@Zorcrown.ZorcrownGames>
2021-04-07 12:19:41 -0500

    cambios en validacion de email

--- src/routes/usuario.ts
+++ src/routes/usuario.ts
@@ -3,14 +3,20 @@ import express from 'express'
 
 export const UsuarioRouter = express.Router()
 
-UsuarioRouter.post('/add',async (req,res)=>{
+UsuarioRouter.post('/add', (req,res)=>{
     const user = req.body
-    try{
-        let result = await UsuarioController.addUser(user)
-        res.status(200).json(result)
-    }catch(err){
-        console.log(err);
-        res.status(500).json(err);
-    }
+    UsuarioController.addUser(user).then(resp=>{
+        res.status(200).json(resp)
+    },err=>{
+        
+        const {detail} = err.parent
+        if (detail){
+            if(detail.indexOf('Ya existe la llave (email_us)=')===0){
+                res.status(500).send("Correo electronico ya registrado")
+                return;
+            }
+        }
+        res.status(500).json(err)
+    })
 })
 
